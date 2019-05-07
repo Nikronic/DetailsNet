ee263f140f1de952104fd8307daccb8dfcbcf111
M. Doosti Lakhani <nikan.doosti@outlook.com>
2019-05-07 14:28:42 +0430

    add DiscriminatorOne and DiscriminatorTwo models

--- Discriminators.py
+++ Discriminators.py
@@ -76,8 +76,53 @@ class DiscriminatorOne(nn.Module):
 
         super(DiscriminatorOne, self).__init__()
         # TODO input size is ARBITRARY for now and must be calculated exactly!
+        self.cl = CL(input_channel=input_channel, output_channel=128, kernel_size=4, stride=2, padding=1)
+        self.cbl0 = CBL(input_channel=128, output_channel=256, kernel_size=4, stride=2, padding=1)
+        self.cbl1 = CBL(input_channel=256, output_channel=512, kernel_size=4, stride=2, padding=1)
+        self.cbl2 = CBL(input_channel=512, output_channel=1024, kernel_size=4, stride=2, padding=1)
+        self.cbl3 = CBL(input_channel=1024, output_channel=2048, kernel_size=4, stride=2, padding=1)
+
+        self.final = C(input_channel=2048, output_channel=output_channel, kernel_size=1, stride=1, padding=0)
 
     def forward(self, x):
-        pass
+        x = self.cl(x)
+        x = self.cbl0(x)
+        x = self.cbl1(x)
+        x = self.cbl2(x)
+        x = self.cbl3(x)
+        x = self.final(x)
+        return x
+
+
+class DiscriminatorTwo(nn.Module):
+    def __init__(self, input_channel, output_channel=1):
+        """
+        Consists of a CL module followed by repetitive CBL modules and finally a C class
+        to match the final needed classes.
+
+        :param input_channel: number of input channels of input images to network.
+        :param output_channel: number of output channels of input images to network.
+        """
+
+        super(DiscriminatorTwo, self).__init__()
+        # TODO input size is ARBITRARY for now and must be calculated exactly!
+        self.cl = CL(input_channel=input_channel, output_channel=128, kernel_size=5, stride=2, padding=0)
+        self.cbl0 = CBL(input_channel=128, output_channel=256, kernel_size=5, stride=2, padding=0)
+        self.cbl1 = CBL(input_channel=256, output_channel=512, kernel_size=5, stride=2, padding=0)
+        self.cbl2 = CBL(input_channel=512, output_channel=1024, kernel_size=5, stride=2, padding=0)
+        self.cbl3 = CBL(input_channel=1024, output_channel=2048, kernel_size=5, stride=2, padding=0)
+
+        self.final = C(input_channel=2048, output_channel=output_channel, kernel_size=4, stride=1, padding=0)
+
+    def forward(self, x):
+        x = self.cl(x)
+        x = self.cbl0(x)
+        x = self.cbl1(x)
+        x = self.cbl2(x)
+        x = self.cbl3(x)
+        x = self.final(x)
+        return x
+
+
 
 # TODO add documentation to submodules for kernel_Size, stride and padding arguments.
