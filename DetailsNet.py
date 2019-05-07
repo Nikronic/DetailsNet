8775fc52e64ea6a69990a1a20df9a3b3befc10c8
M. Doosti Lakhani <nikan.doosti@outlook.com>
2019-05-07 12:36:49 +0430

    add submodules of generator

--- DetailsNet.py
+++ DetailsNet.py
@@ -3,20 +3,42 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class CBR(nn.Module):
-    def __init__(self, input_channel, output_channel):
+class CL(nn.Module):
+    def __init__(self, input_channel, output_channel, kernel_size=4, stride=2, padding=1):
         """
-        It consists of the 5x5 convolutions with stride=1, padding=2, and a batch normalization, followed by
-        a rectified linear unit (ReLU)
+        It consists of the 4x4 convolutions with stride=2, padding=1, each followed by
+        a leaky rectified linear unit (Leaky ReLU)
+
+        :param input_channel: input channel size
+        :param output_channel: output channel size
+        """
+
+        assert (input_channel > 0 and output_channel > 0)
+
+        super(CL, self).__init__()
+        layers = [nn.Conv2d(input_channel, output_channel, kernel_size=kernel_size, stride=stride, padding=padding),
+                  nn.LeakyReLU(0.2)]
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+# %%
+class CBL(nn.Module):
+    def __init__(self, input_channel, output_channel, kernel_size=4, stride=2, padding=1):
+        """
+        It consists of the 4x4 convolutions with stride=2, padding=1, and a batch normalization, followed by
+        a leaky rectified linear unit (ReLU)
 
         :param input_channel: input channel size
         :param output_channel: output channel size
         """
         assert (input_channel > 0 and output_channel > 0)
 
-        super(CBR, self).__init__()
-        layers = [nn.Conv2d(input_channel, output_channel, kernel_size=5, stride=1, padding=2),
-                  nn.BatchNorm2d(num_features=output_channel), nn.ReLU(0.2)]
+        super(CBL, self).__init__()
+        layers = [nn.Conv2d(input_channel, output_channel, kernel_size=kernel_size, stride=stride, padding=padding),
+                  nn.BatchNorm2d(num_features=output_channel), nn.LeakyReLU(0.2)]
         self.layers = nn.Sequential(*layers)
 
     def forward(self, x):
@@ -41,40 +63,54 @@ class C(nn.Module):
         return self.layer(x)
 
 
-class EdgeNet(nn.Module):
-    def __init__(self, input_channels=3, output_channels=1):
+class ResidualBlock(nn.Module):
+    def __init__(self, input_channel, output_channel, kernel_size=3, stride=1, padding=1):
         """
-        A simple convolutional neural network to learn edges using Canny edge detector
+        A residual block contains a sequence of CBL and CL classes with same size of stride, padding and kernel size.
+
+        :param input_channel: number of input channels of input images to network.
+        :param output_channel: number of output channels of output images of network.
+        :param stride: stride size of CBL and CL modules
+        :param kernel_size: kernel_size of CBL and CL modules
+        :param padding: padding size of CBL and CL modules
+        """
+
+        super(ResidualBlock, self).__init__()
+        self.input_channel = input_channel
+        self.output_channel = output_channel
+        self.stride = stride
+        self.kernel_size = kernel_size
+        self.padding = padding
+
+        # blocks
+        self.cbl = CBL(input_channel, output_channel, kernel_size, stride, padding)
+        self.cl = CL(input_channel, output_channel, kernel_size, stride, padding)
+
+    def forward(self, x):
+        out = self.cbl(x)
+        out = self.cl(out)
+        return out
+
+
+class DetailsNet(nn.Module):
+    def __init__(self, input_channels=64, output_channels=3):
+        """
+        The generator of GAN networks contains repeated residual blocks and C block at the end.
 
         :param input_channels: number of input channels of input images to network.
         :param output_channels: number of output channels of output images of network.
         """
 
-        super(EdgeNet, self).__init__()
+        super(DetailsNet, self).__init__()
         self.input_channels = input_channels
         self.output_channels = output_channels
 
-        self.cbr0 = CBR(input_channels, 32)
-        self.cbr1 = CBR(32, 32)
-        self.cbr2 = CBR(32, 32)
-        self.cbr3 = CBR(32, 32)
-        self.cbr4 = CBR(32, 32)
-
-        # final
-        self.final = C(32, self.output_channels)
+        self.c = C(input_channel=64, output_channel=3)
 
     def forward(self, x):
-        c = self.cbr0(x)
-        c = self.cbr1(c)
-        c = self.cbr2(c)
-        c = self.cbr3(c)
-        c = self.cbr4(c)
-        
-        c = self.final(c)
-        return c
+        pass
 
 
-#
 # from tensorboardX import SummaryWriter
 #
 # dummy_input = (torch.zeros(1, 3, 256, 256),)
