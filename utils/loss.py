56730b6396044af3bf80baf3f228c3c7eec80780
M. Doosti Lakhani <nikan.doosti@outlook.com>
2019-05-18 02:24:05 +0430

    rename loss to DetailsLoss fo consistency
    Took 12 minutes

--- utils/loss.py
+++ utils/loss.py
@@ -2,12 +2,12 @@
 import torch.nn as nn
 
 
-class EdgeLoss(nn.Module):
+class DetailsLoss(nn.Module):
     def __init__(self):
         """
-        Return Binary Entropy Loss with mean of all losses in each mini-batch
+        Return MSE Loss with mean of all losses in each mini-batch
         """
-        super(EdgeLoss, self).__init__()
+        super(DetailsLoss, self).__init__()
         self.MSE_loss = nn.MSELoss(reduction='mean')
 
     def forward(self, y, y_pred):
